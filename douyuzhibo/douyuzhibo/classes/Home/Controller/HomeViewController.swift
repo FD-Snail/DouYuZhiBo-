9af263e5d6bfa558b8fc5e3cd338543f0f4a1a38
FDSnail <wukeng1122@126.com>
2017-01-08 16:55:15 +0800

    测试git添加文件

---
+++ douyuzhibo/douyuzhibo/classes/Home/Controller/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  douyuzhibo
+//
+//  Created by wukeng on 17/1/8.
+//  Copyright © 2017年 吴铿. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.backgroundColor = UIColor.orange
+    }
+}
---
+++ douyuzhibo/douyuzhibo/classes/Tools/Category/WuK_NavigationBar.swift
@@ -0,0 +1,16 @@
+//
+//  WuK_NavigationBar.swift
+//  douyuzhibo
+//
+//  Created by wukeng on 17/1/8.
+//  Copyright © 2017年 吴铿. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationBar{
+    convenience init(color : UIColor){
+        self.init()
+        self.backgroundColor = color
+    }
+}
